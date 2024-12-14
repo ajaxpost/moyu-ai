34837259dc0c74ec7d3de0a010e969aca71cd151
houdekang <houdekang.ztrz@bytedance.com>
2024-12-14 16:18:33 +0800

    feat: 新增 auth 功能

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/") {
+    // return NextResponse.rewrite(new URL("/", req.url));
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
