3fdf1d90f1c24440a4d21879b61de72c5726dc04
houdekang <houdekang.ztrz@bytedance.com>
2025-05-07 18:31:34 +0800

    feat: 图片支持放大查看

--- extensions/image-block/components/image-block-view.tsx
+++ extensions/image-block/components/image-block-view.tsx
@@ -3,17 +3,20 @@ import { Spinner } from "@/components/ui/spiner";
 import { cn } from "@/lib/utils";
 import { Node } from "@tiptap/pm/model";
 import { Editor, NodeViewWrapper } from "@tiptap/react";
-import { useCallback, useRef } from "react";
+import { useCallback, useMemo, useRef } from "react";
+import { PhotoProvider, PhotoView } from "react-image-previewer";
+import { SlideToolbar, CloseButton } from "react-image-previewer/ui";
 
 interface ImageBlockViewProps {
   editor: Editor;
   getPos: () => number;
   node: Node;
   updateAttributes: (attrs: Record<string, string>) => void;
+  selected: boolean;
 }
 
 export const ImageBlockView = (props: ImageBlockViewProps) => {
-  const { editor, getPos, node } = props as ImageBlockViewProps & {
+  const { editor, getPos, node, selected } = props as ImageBlockViewProps & {
     node: Node & {
       attrs: {
         src: string;
@@ -34,21 +37,67 @@ export const ImageBlockView = (props: ImageBlockViewProps) => {
     editor.commands.setNodeSelection(getPos());
   }, [getPos, editor.commands]);
 
+  const getAllImages = useCallback(() => {
+    const images: Node[] = [];
+    editor.state.doc.descendants((node) => {
+      if (node.type.name === "imageBlock") {
+        images.push(node);
+      }
+    });
+    return images;
+  }, [editor.state.doc]);
+
+  const images = useMemo(() => getAllImages(), [getAllImages]);
+
   return (
     <NodeViewWrapper>
       <div
-        className={`${wrapperClassName} relative`}
+        className={cn(`${wrapperClassName} relative`, {
+          "cursor-zoom-in": selected,
+        })}
         style={{ width: node.attrs.width }}
       >
         <div contentEditable={false} ref={imageWrapperRef}>
-          <img
-            className={cn("block", {
-              "opacity-[0.2]": loading,
-            })}
-            src={src}
-            alt=""
-            onClick={onClick}
-          />
+          {selected ? (
+            <PhotoProvider
+              overlayRender={(props) => {
+                const { onClose } = props;
+                return (
+                  <>
+                    <SlideToolbar {...props} />
+                    <CloseButton onClick={onClose} />
+                  </>
+                );
+              }}
+            >
+              {images.map((item, index) => {
+                const viewSrc = item.attrs.src;
+                return (
+                  <PhotoView key={index} src={viewSrc}>
+                    {viewSrc === src ? (
+                      <img
+                        className={cn("block", {
+                          "opacity-[0.2]": loading,
+                        })}
+                        src={viewSrc}
+                        alt=""
+                        onClick={onClick}
+                      />
+                    ) : undefined}
+                  </PhotoView>
+                );
+              })}
+            </PhotoProvider>
+          ) : (
+            <img
+              className={cn("block", {
+                "opacity-[0.2]": loading,
+              })}
+              src={src}
+              alt=""
+              onClick={onClick}
+            />
+          )}
         </div>
         {loading && (
           <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
